4292e0b8d390e9e205bbd5a285d707d947efd0b0
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-05-19 22:03:13 +0530

    removed bugs

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const datapoint = './data/HDFC.json';
-
 //retrieving data entered by user.
 var input_ifsc = document.getElementById('ifsc_code');
-console.log(input_ifsc.value);
 
 //creating show_details variable to change display type of output from 'none' to 'block'
 var show_details = document.getElementById('output-section');
@@ -26,28 +23,41 @@ var output = document.getElementById('output');
 
 //main function to display and retrieve data from json
 async function getData() {
+
     var input = document.getElementById('ifsc_code').value;
+    
+    if (input == "") {
+      alert("please input IFSC");
+      return;
+    }
+
+    let datapoint = `https://ifsc.razorpay.com/${input}`;
     
     const response = await fetch(datapoint);
     const data = await response.json();
 
-    var display = data[input];
-    console.log(display);
+    if (data !== 'Not Found') {
+        var display = data;
 
-    bank_name.innerText = display.BANK;
-    ifsc.innerText = display.IFSC;
-    micr.innerText = display.MICR;
-    branch.innerText = display.BRANCH;
-    address.innerText = display.ADDRESS;
-    contact.innerText = display.CONTACT;
-    upi.innerText = display.UPI;
-    rtgs.innerText = display.RTGS;
-    neft.innerText = display.NEFT;
-    imps.innerText = display.IMPS;
-    city.innerText = display.CITY;
-    district.innerText = display.DISTRICT;
-    swift.innerText = display.SWIFT;
+        bank_name.innerText = display.BANK;
+        ifsc.innerText = display.IFSC;
+        micr.innerText = display.MICR;
+        branch.innerText = display.BRANCH;
+        address.innerText = display.ADDRESS;
+        contact.innerText = display.CONTACT;
+        upi.innerText = display.UPI;
+        rtgs.innerText = display.RTGS;
+        neft.innerText = display.NEFT;
+        imps.innerText = display.IMPS;
+        city.innerText = display.CITY;
+        district.innerText = display.DISTRICT;
+        swift.innerText = display.SWIFT;
 
-    show_details.style.display = "block";
-    
+        show_details.style.display = "block";
+    } 
+    else {
+        alert('No such IFSC code available, please enter valid IFSC code');
+    }
 }
+
+document.getElementById('submit').addEventListener('click' , getData);
